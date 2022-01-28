e17fbc68c4e1f9d39a1fcea960d8832a70833660
Chandra <tech.vchandramohan@gmail.com>
2022-01-28 09:58:40 -0500

    BranchTwo - Base version

--- src/main/java/com/sample/DevClass.java
+++ src/main/java/com/sample/DevClass.java
@@ -5,5 +5,6 @@ public class DevClass {
         System.out.println("Adding Branch One - Changes");
         int a = 10, b=1, c = a/b;
         System.out.println("Fixed Null Pointer error!");
+        // Working onb branch two
     }
 }
