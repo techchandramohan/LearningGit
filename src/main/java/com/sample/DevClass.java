09745c45009f3977d4d7e4160c93a542a06c023e
Chandra <tech.vchandramohan@gmail.com>
2022-01-27 22:51:30 -0500

    Commit with Comments

--- src/main/java/com/sample/DevClass.java
+++ src/main/java/com/sample/DevClass.java
@@ -3,5 +3,6 @@ package com.sample;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        // Adding some comments here
     }
 }
--- src/test/java/com/sample/TestDevClass.java
+++ src/test/java/com/sample/TestDevClass.java
@@ -3,5 +3,6 @@ package com.sample;
 public class TestDevClass {
     public static void main(String[] args) {
         System.out.println("Hello Test!");
+        // Added some more Test comments
     }
 }
