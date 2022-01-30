5dce8c884c040cf326fbfe7e4e0dbfc7293cd13d
Chandra <tech.vchandramohan@gmail.com>
2022-01-30 11:40:55 -0500

    User Story 1001 - Base Comment

--- src/main/java/com/sample/DevClass.java
+++ src/main/java/com/sample/DevClass.java
@@ -8,5 +8,7 @@ public class DevClass {
         // Working onb branch two
         System.out.println("Welcome to Branch Two");
         // I am planning to do some more change while merging in to Branch One.
+
+        // New changes related to user Story 1001
     }
 }
